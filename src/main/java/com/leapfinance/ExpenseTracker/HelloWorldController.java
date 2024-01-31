54925c81cf8edaa8b332ccb67e09e853c1cacb21
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-02-01 00:12:59 +0530

    code base initialized

---
+++ src/main/java/com/leapfinance/ExpenseTracker/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.leapfinance.ExpenseTracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    
+    @GetMapping(path = "/hello")
+    public String helloworld() {
+        return "Hellow wosddsds";
+    }
+}
